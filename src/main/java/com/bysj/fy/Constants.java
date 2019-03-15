f32480dc1f9970265192c8c4a71f8d5f5f2b84d6
chengchuanqi <cqcheng@akazam.com>
2019-03-15 17:31:51 +0800

    新增百度翻译接口

---
+++ src/main/java/com/bysj/fy/Constants.java
@@ -0,0 +1,13 @@
+package com.bysj.fy;
+
+public interface Constants {
+
+    interface BAIDU_INFO{
+        String APP_ID = "20190315000277475";
+        String SECURITY_KEY = "admHn5vUhBab7ulUgENy";
+    }
+
+    interface BAIDU_URL{
+
+    }
+}
